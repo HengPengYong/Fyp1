4aa5c085eb28f15c18a2ff70bbe6b83b17a8b362
HengPengYong <120778920+HengPengYong@users.noreply.github.com>
2024-02-25 17:45:29 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,3 +38,5 @@ st.altair_chart(alt.Chart(df, height=700, width=700)
         color=alt.Color("idx", legend=None, scale=alt.Scale()),
         size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
     ))
+
+st.number_input('Pick',0,10)
