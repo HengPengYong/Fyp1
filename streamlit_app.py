25d926e7efa59b59bdb6dbd5c5c833798c12f2a1
HengPengYong <120778920+HengPengYong@users.noreply.github.com>
2024-02-27 18:08:46 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,20 +7,6 @@ from PIL import Image
 import streamlit as st
 
 def main():
-        # Custom CSS to set the background color
-    main_bg = "#4CAF50"  # Light blue color
-    main_bg_css = f"background-color: {main_bg};"
-    st.markdown(
-        f"""
-        <style>
-            .reportview-container {{
-                {main_bg_css}
-            }}
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
-
     
     st.title("Multiclass Classification with Many Categories")
     
