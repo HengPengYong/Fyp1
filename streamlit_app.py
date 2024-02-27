ed65bf9377e464249c2c0daa765d4219403706bc
HengPengYong <120778920+HengPengYong@users.noreply.github.com>
2024-02-27 17:23:27 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ from PIL import Image
 st.title('Entire Architecture Diagram')
 
 # Load the image from file
- image = Image.open('.devcontainer/Screenshot 2021-06-02 141726.png')
+image = Image.open('.devcontainer/Screenshot 2021-06-02 141726.png')
 # Display the image
 
 
