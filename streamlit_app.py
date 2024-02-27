7ac48ac50bb197ced1b0bd344e0c718b551da918
HengPengYong <120778920+HengPengYong@users.noreply.github.com>
2024-02-27 18:02:35 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ from PIL import Image
 import streamlit as st
 
 def main():
-    st.title("Thesis Defense Presentation")
+    st.title("Multiclass Classification with Many Categories")
     
     # Sidebar navigation
     option = st.sidebar.radio('Navigation', ['Home', 'Technique 1', 'Technique 2'])
@@ -24,14 +24,14 @@ def display_home():
     st.subheader("Date: February 27, 2024")  # Replace with the current date
     
     # Names of candidate, supervisor, and moderator
-    candidate_name = "John Doe"
-    supervisor_name = "Dr. Jane Smith"
-    moderator_name = "Prof. Michael Johnson"
+    candidate_name = "Heng Peng Yong"
+    supervisor_name = "Ts. Dr. Goh Hui Ngo"
+    moderator_name = "Prof. Ts. Dr. Ting Choo Yee"
     
     st.subheader("Names:")
     st.write(f"- Candidate: {candidate_name}")
-    st.write(f"- Supervisor: {candidate_name}")
-    st.write(f"- Moderator: {candidate_name}")
+    st.write(f"- Supervisor: {supervisor_name}")
+    st.write(f"- Moderator: {moderator_name}")
 
 def display_technique_1():
     st.subheader("Technique 1 Content Goes Here")
